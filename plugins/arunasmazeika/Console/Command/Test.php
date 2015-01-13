7ca708b1395871acac8d9ee6861cf7f2b8e6dabc
Arunas Mazeika <arunas@timble.net>
2015-01-13 12:10:08 +0100

    Initial commit.

---
+++ plugins/arunasmazeika/Console/Command/Test.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2007 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		Mozilla Public License, version 2.0
+ * @link		http://github.com/joomlatools/joomla-console for the canonical source repository
+ */
+
+namespace Arunasmazeika\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Test extends Command
+{
+    protected function configure()
+    {
+        $this->setName('test')->setDescription('Just a test command');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<message>It works!</message>');
+    }
+}
